440a738ecaefb31270d2fdf35964f70de9f42176
rory1132 <mchon@connect.ust.hk>
2022-11-25 05:24:11 +0800

    Initial commit

---
+++ src/main/java/hk/ust/comp3021/replay/ReplaySokobanGame.java
@@ -0,0 +1,282 @@
+package hk.ust.comp3021.replay;
+
+
+import hk.ust.comp3021.actions.ActionResult;
+import hk.ust.comp3021.actions.Exit;
+import hk.ust.comp3021.game.AbstractSokobanGame;
+import hk.ust.comp3021.game.GameState;
+import hk.ust.comp3021.game.InputEngine;
+import hk.ust.comp3021.game.RenderingEngine;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import static hk.ust.comp3021.utils.StringResources.*;
+
+/**
+ * A thread-safe Sokoban game.
+ * The game should be able to run in a separate thread, and games running in parallel should not interfere with each other.
+ * <p>
+ * The game can run in two modes:
+ * 1. {@link Mode#ROUND_ROBIN} mode: all input engines take turns to perform actions, starting from the first specified input engine.
+ * Example: suppose there are two input engines, A and B, whose actions are [R, L], [R, L], respectively.
+ * In this mode, the game will perform the following actions in order: A.R, B.R, A.L, B.L.
+ * 2. {@link Mode#FREE_RACE} mode: all input engines perform actions simultaneously. The actions processed can be in any order.
+ * There could be a chance that two runs of the same game process actions in different orders.
+ * <p>
+ * {@link hk.ust.comp3021.Sokoban#replayGame(int, String, Mode, int, String[])} runs multiple games in parallel.
+ */
+public class ReplaySokobanGame extends AbstractSokobanGame {
+    /**
+     * Mode of scheduling actions among input engines.
+     */
+    public enum Mode {
+        /**
+         * All input engines take turns to perform actions, starting from the first specified input engine.
+         */
+        ROUND_ROBIN,
+
+        /**
+         * All input engines perform actions concurrently without enforcing the order.
+         */
+        FREE_RACE,
+    }
+
+    protected final Mode mode;
+    /**
+     * Indicated the frame rate of the rendering engine (in FPS).
+     */
+    protected final int frameRate;
+
+    /**
+     * Default frame rate.
+     */
+    protected static final int DEFAULT_FRAME_RATE = 60;
+
+    /**
+     * The list of input engines to fetch inputs.
+     */
+    protected final List<? extends InputEngine> inputEngines;
+
+    /**
+     * The rendering engine to render the game status.
+     */
+    protected final RenderingEngine renderingEngine;
+
+    /**
+     * Create a new instance of ReplaySokobanGame.
+     * Each input engine corresponds to an action file and will produce actions from the action file.
+     *
+     * @param mode            The mode of the game.
+     * @param frameRate       Rendering fps.
+     * @param gameState       The game state.
+     * @param inputEngines    the input engines.
+     * @param renderingEngine the rendering engine.
+     */
+    public ReplaySokobanGame(
+            @NotNull Mode mode,
+            int frameRate,
+            @NotNull GameState gameState,
+            @NotNull List<? extends InputEngine> inputEngines,
+            @NotNull RenderingEngine renderingEngine
+    ) {
+        super(gameState);
+        if (inputEngines.size() == 0)
+            throw new IllegalArgumentException("No input engine specified");
+        this.mode = mode;
+        this.frameRate = frameRate;
+        this.renderingEngine = renderingEngine;
+        this.inputEngines = inputEngines;
+    }
+
+    /**
+     * @param gameState       The game state.
+     * @param inputEngines    the input engines.
+     * @param renderingEngine the rendering engine.
+     */
+    public ReplaySokobanGame(
+            @NotNull GameState gameState,
+            @NotNull List<? extends InputEngine> inputEngines,
+            @NotNull RenderingEngine renderingEngine) {
+        this(Mode.FREE_RACE, DEFAULT_FRAME_RATE, gameState, inputEngines, renderingEngine);
+    }
+
+    // TODO: add any method or field you need.
+    public class ABC {
+        int currentIndex = 0;
+        void plus() {
+            currentIndex++;
+        }
+    }
+
+    int colIndex = 0;
+    ABC abc;
+    int count = 0;
+    //int[] indexArr;
+
+    /**
+     * The implementation of the Runnable for each input engine thread.
+     * Each input engine should run in a separate thread.
+     * <p>
+     * Assumption:
+     * 1. the last action fetch-able from the input engine is always an {@link Exit} action.
+     * <p>
+     * Requirements:
+     * 1. All actions fetched from input engine should be processed in the order they are fetched.
+     * 2. All actions before (including) the first {@link Exit} action should be processed
+     * (passed to {@link this#processAction} method).
+     * 3. Any actions after the first {@link Exit} action should be ignored
+     * (not passed to {@link this#processAction}).
+     */
+    private class InputEngineRunnable implements Runnable {
+        private final int index;
+        private final InputEngine inputEngine;
+        boolean firstExit = false;
+
+
+        private InputEngineRunnable(int index, @NotNull InputEngine inputEngine) {
+            this.index = index;
+            this.inputEngine = inputEngine;
+        }
+
+        @Override
+        public void run() {
+            // TODO: modify this method to implement the requirements.
+
+            if (mode==Mode.ROUND_ROBIN) {
+
+                var copy = index;
+
+                    while (!shouldStop() || !firstExit) {
+
+                        final var action = inputEngine.fetchAction();
+
+                        if (action instanceof Exit) {
+                            firstExit = true;
+                            try {
+                                Thread.sleep(30);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+
+                        while ((abc.currentIndex % colIndex) != copy&&count!=(colIndex-1)) {
+                            try {
+                                Thread.sleep(30);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                        System.out.println(action);
+                        final var result = processAction(action);
+                        abc.plus();
+
+
+                        if (result instanceof ActionResult.Failed failed) {
+                            renderingEngine.message(failed.getReason());
+                        }
+
+
+                    }
+                    count++;
+
+            }
+
+
+            if (mode == Mode.FREE_RACE) {
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                while (!shouldStop() || !firstExit) {
+                    final var action = inputEngine.fetchAction();
+                    if (action instanceof Exit) {
+                        firstExit = true;
+                        try {
+                            Thread.sleep(30);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    final var result = processAction(action);
+                    if (result instanceof ActionResult.Failed failed) {
+                        renderingEngine.message(failed.getReason());
+                    }
+                }
+
+            }
+        }
+    }
+
+    /**
+     * The implementation of the Runnable for the rendering engine thread.
+     * The rendering engine should run in a separate thread.
+     * <p>
+     * Requirements:
+     * 1. The game map should be rendered at least once before any action is processed (the initial state should be rendered).
+     * 2. The game map should be rendered after the last action is processed (the final state should be rendered).
+     */
+    private class RenderingEngineRunnable implements Runnable {
+        /**
+         * NOTE: You are NOT allowed to use {@link java.util.Timer} or {@link java.util.TimerTask} in this method.
+         * Please use a loop with {@link Thread#sleep(long)} instead.
+         */
+        @Override
+        public void run() {
+            // TODO: modify this method to implement the requirements.
+            do {
+                final var undoQuotaMessage = state.getUndoQuota()
+                    .map(it -> String.format(UNDO_QUOTA_TEMPLATE, it))
+                    .orElse(UNDO_QUOTA_UNLIMITED);
+                renderingEngine.message(undoQuotaMessage);
+                renderingEngine.render(state);
+                try {
+                    Thread.sleep(950/frameRate);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } while (!shouldStop());
+        }
+    }
+
+    /**
+     * Start the game.
+     * This method should spawn new threads for each input engine and the rendering engine.
+     * This method should wait for all threads to finish before return.
+     */
+    @Override
+    public void run() {
+        // TODO
+        if (mode==Mode.ROUND_ROBIN) {
+            colIndex = inputEngines.size();
+            abc = new ABC();
+        }
+
+        Thread[] ths = new Thread[inputEngines.size()+1];
+        for (int i = 0; i < inputEngines.size(); i++) {
+            ths[i+1] = new Thread(new InputEngineRunnable(i,inputEngines.get(i)));
+        }
+        ths[0] = new Thread(new RenderingEngineRunnable());
+
+
+
+        for (final var th :
+                ths) {
+            th.start();
+        }
+        for (final var th :
+                ths) {
+            try {
+                th.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+    }
+
+
+
+}
